d125139aa869a11068321489efda3710737ad2b7
paicrush <dcdc07411@gmail.com>
2020-09-06 13:44:12 +0700

    100%

--- app/utility/validator.py
+++ app/utility/validator.py
@@ -1,4 +1,12 @@
 def validate_name(name):
+    checkname = ["1", "2", "3", "4", "5", "6", "7",
+                 "8", "9", "0", "!", "@", "#", "$", " "]
+    name = str(name)
+    if len(name) == 0:
+        return False
+    for i in name:
+        if i in checkname:
+            return False
     return True
 
 
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -3,8 +3,23 @@ from app.utility.validator import validate_name, validate_id, validate_phone_num
 
 
 class TestUtility(unittest.TestCase):
+    def test_validate_name_with_valid_input_int(self):
+        self.assertEqual(False, validate_name("1234"))
+
     def test_validate_name_with_valid_input(self):
-        self.assertEqual(True, validate_name("hanif"))
+        self.assertEqual(True, validate_name("ปอซ่า"))
+
+    def test_validate_name_with_valid_input_string(self):
+        self.assertEqual(False, validate_name("ชายปอ007"))
+
+    def test_validate_name_with_valid_input_string_char(self):
+        self.assertEqual(False, validate_name("มีไรหรอ!"))
+
+    def test_validate_name_with_valid_input_string_char_space(self):
+        self.assertEqual(False, validate_name("xkp ss"))
+
+    def test_validate_name_with_valid_input_string_char_None(self):
+        self.assertEqual(False, validate_name(""))
 
 
 if __name__ == '__main__':
